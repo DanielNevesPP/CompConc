fc9ce9a0790778afa0ed9d62933bded88db5c641
DanielNevesPP <85492821+DanielNevesPP@users.noreply.github.com>
2022-04-25 22:29:06 -0300

    Add files via upload

---
+++ Trabalho2CompConc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "timer.h"
+
+int main(int argc, char* argv[]){
+
+    int dim, nThreads;
+
+    if(argc  < 3){
+      printf("Digite: %s <dimensao da matriz> <numero de threads>\n", argv[0]);
+      return 1;
+    }
+
+    dim = atoi(argv[1]);
+    nThreads = atoi(argv[2]);
+
+}
